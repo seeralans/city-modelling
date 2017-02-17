e9a8882822575dfa6e6590a34305162ca9366114
Ben Menzies <bm14539@my.bristol.ac.uk>
2017-02-17 15:18:28 +0000

    Ben Tambling wrote some code and shared it on FB so Ben Menzies push it to the master!

---
+++ LOADSADATA.m
@@ -0,0 +1,35 @@
+function Cen = LOADSADATA()
+Cen{1} = importdata('Reduced_Census/Census/ks101ew');
+Cen{2} = importdata('Reduced_Census/Census/ks102ew');
+Cen{3} = importdata('Reduced_Census/Census/ks103ew');
+Cen{4} = importdata('Reduced_Census/Census/ks104ew');
+Cen{5} = importdata('Reduced_Census/Census/ks105ew');
+Cen{6} = importdata('Reduced_Census/Census/ks106ew');
+Cen{7} = importdata('Reduced_Census/Census/ks107ew');
+Cen{8} = importdata('Reduced_Census/Census/ks201ew');
+Cen{9} = importdata('Reduced_Census/Census/ks202ew');
+Cen{10} = importdata('Reduced_Census/Census/ks204ew');
+Cen{11} = importdata('Reduced_Census/Census/ks206ew');
+Cen{12} = importdata('Reduced_Census/Census/ks209ew');
+Cen{13} = importdata('Reduced_Census/Census/ks301ew');
+Cen{14} = importdata('Reduced_Census/Census/ks401ew');
+Cen{15} = importdata('Reduced_Census/Census/ks402ew');
+Cen{16} = importdata('Reduced_Census/Census/ks404ew');
+Cen{17} = importdata('Reduced_Census/Census/ks405ew');
+Cen{18} = importdata('Reduced_Census/Census/ks501ew');
+Cen{19} = importdata('Reduced_Census/Census/ks601ew');
+Cen{20} = importdata('Reduced_Census/Census/ks602ew');
+Cen{21} = importdata('Reduced_Census/Census/ks603ew');
+Cen{22} = importdata('Reduced_Census/Census/ks604ew');
+Cen{23} = importdata('Reduced_Census/Census/ks605ew');
+Cen{24} = importdata('Reduced_Census/Census/ks606ew');
+Cen{25} = importdata('Reduced_Census/Census/ks607ew');
+Cen{26} = importdata('Reduced_Census/Census/ks608ew');
+Cen{27} = importdata('Reduced_Census/Census/ks609ew');
+Cen{28} = importdata('Reduced_Census/Census/ks610ew');
+Cen{29} = importdata('Reduced_Census/Census/ks611ew');
+Cen{30} = importdata('Reduced_Census/Census/ks612ew');
+Cen{31} = importdata('Reduced_Census/Census/ks613ew');
+return
+
+
