a2be5551b235145bfbbcf147377804cdb48d3870
lbothwell <lb14292@my.bristol.ac.uk>
2017-02-21 12:55:30 +0000

    Cleaned up Ben's code, with score included

--- Clustering3.m
+++ Clustering3.m
@@ -1,32 +1,8 @@
-clear;
-
-Cen = loadData; %Function just loads all the KS data files
 
 load Coords
 load CoordsCityCouncil
-
-num2 = 31; %Choose how many files to go through
-
-cenData = [1:num2];
-
-count = 1;
-Tot=0;
-
-for i = cenData
-    Tot=Tot+size(Cen{i}.data,2);
-end
-
-allData = zeros(3485,Tot);
-
-for i = cenData 
-    for j = 1:size(Cen{i}.data,2)
-        allData(:,count) = Cen{i}.data(:,j);
-        count = count+1;
-    end
-end
-
-
-[IDX C] = kmeans(allData(:,351:354),3);
+k = 3; % no. of clusters
+[IDX, C] = kmeans(score(:,[1 2 3 4]),k);
 
 for i = 1:3485
     
@@ -39,25 +15,9 @@ for i = 1:3485
     else 
         col = 0;
     end
-    patch(a,b,[col col col])
-    %patch(a,b,[IDX(i)-1 IDX(i)-1 IDX(i)-1])
-    %patch(a,b,[1-((WantedVariable(i)-MaxDat)/(MinDat-MaxDat)) 1-((WantedVariable(i)-MaxDat)/(MinDat-MaxDat)) 1-((WantedVariable(i)-MaxDat)/(MinDat-MaxDat))]);
-    %patch(a,b,[(WantedVariable(i)-MaxDat)/(MinDat-MaxDat) (WantedVariable(i)-MaxDat)/(MinDat-MaxDat) (WantedVariable(i)-MaxDat)/(MinDat-MaxDat)])%(WantedVariable(i)-MinDat)/(MaxDat-MinDat)(WantedVariable(i)-MaxDat)/(MinDat-MaxDat)
+    patch(a,b,[IDX(i)/k 1 IDX(i)/k])
     hold on
 end    
-% IDX = 0;
-% for i = 1:3485
-%     
-%     a = Coords{i}(:,1);
-%     b = Coords{i}(:,2);
-%     if WantedVariable(i) < 50
-%         IDX = 1;
-%     else
-%         IDX = 0;
-%     end
-%     patch(a,b,[IDX IDX IDX])%(WantedVariable(i)-MinDat)/(MaxDat-MinDat)(WantedVariable(i)-MaxDat)/(MinDat-MaxDat)
-%     hold on
-% end  
 
 %Uncomment this to plot bristol city council border
 %plot(CoordsCityCouncil(:,1),CoordsCityCouncil(:,2),'b','linewidth',3)
