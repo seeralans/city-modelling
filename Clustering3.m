201ad034fe64eb10df8c087e175def1291d0a380
lbothwell <lb14292@my.bristol.ac.uk>
2017-02-24 13:21:23 +0000

    Changedd starting means

--- Clustering3.m
+++ Clustering3.m
@@ -2,20 +2,20 @@
 load Coords
 load CoordsCityCouncil
 k = 3; % no. of clusters
-[IDX, C] = kmeans(score(:,[1 2 3 4]),k);
+[IDX, C] = kmeans(normDataMat(:,[351 353 354 384]),k, 'Start',[1 0 0 0; 0 1 0 0; 0 0 1 0]);
 
 for i = 1:length(score)
 
     a = Coords{i}(:,1);
     b = Coords{i}(:,2);
     if IDX(i) == 1
-        col = 1;
+        col = [1 1 1];
     elseif IDX(i) == 2
-        col = 0.5;
+        col = [0.66 0.66 0.66];
     else
-        col = 0;
+        col = [0.33 0.33 0.33];
     end
-    patch(a,b,[IDX(i)/k 1 IDX(i)/k])
+    patch(a,b,col)
     hold on
 end
 
