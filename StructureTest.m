ae4fc9643276e2cabd44b1629c22cea623bc930b
lbothwell <lb14292@my.bristol.ac.uk>
2017-02-27 14:49:41 +0000

    Edit with connection Mat

--- StructureTest.m
+++ StructureTest.m
@@ -1,5 +1,5 @@
 %% New code, compare neighbouring areas
-function Structure = StructureTest(Class, Coords)
+function Structure = StructureTest(Class, Coords, ConnectionMat)
 
 numPoints = length(Coords);
 
@@ -8,25 +8,6 @@ Midpoint = zeros(numPoints,2);
 for i = 1:numPoints
     Midpoint(i,:) = findCentroid(Coords{i});
 end
-ConnectionMat = zeros(numPoints,numPoints);
-DistMat = zeros(numPoints,numPoints);
-for i = 1:numPoints
-    for j = i:numPoints
-        DistMat(i,j) = norm(Midpoint(i, :) - Midpoint(j, :));
-        DistMat(j,i) = DistMat(i,j);
-    end
-end
-
-[ SortDistMat, DistIndex] = sort(DistMat);
-for i = 1:numPoints
-    x = 0;
-    for j = 1:20
-        x = isempty(intersect(Coords{i}, Coords{DistIndex(j, i)}));
-        ConnectionMat(i,DistIndex(j,i)) = 1 - x;
-    end
-end
-
-
 NearClass = zeros(numPoints,1);
 for i = 1:numPoints
     v = find(ConnectionMat(:,i));
