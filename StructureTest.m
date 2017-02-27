6b662e9f42fc3a9b13dcc8650df3680f1dc1083c
seeralan sarvaharman <ss14546@my.bristol.ac.uk>
2017-02-27 13:31:22 +0000

    streamlined code

--- StructureTest.m
+++ StructureTest.m
@@ -1,32 +1,34 @@
 %% New code, compare neighbouring areas
 function Structure = StructureTest(Class, Coords)
-Dist = zeros(3485,1);
-Midpoint = zeros(3485,2);
-for i = 1:3485   
+
+numPoints = length(Coords);
+
+Dist = zeros(numPoints,1);
+Midpoint = zeros(numPoints,2);
+for i = 1:numPoints
     Midpoint(i,:) = findCentroid(Coords{i});
 end
-ConnectionMat = zeros(3485,3485);
-DistMat = zeros(3485,3485);
-for i = 1:3485
-    for j = i:3485
-        DistMat(i,j) = sqrt((Midpoint(i,1)-Midpoint(j,1))^2 + (Midpoint(i,2)-Midpoint(j,2))^2);
+ConnectionMat = zeros(numPoints,numPoints);
+DistMat = zeros(numPoints,numPoints);
+for i = 1:numPoints
+    for j = i:numPoints
+        DistMat(i,j) = norm(Midpoint(i, :) - Midpoint(j, :));
         DistMat(j,i) = DistMat(i,j);
-    end   
+    end
 end
+
 [ SortDistMat, DistIndex] = sort(DistMat);
-for i = 1:3485
-    v = [ Coords{i}(:,1) Coords{i}(:,2)];
+for i = 1:numPoints
     x = 0;
     for j = 1:20
-        u = [ Coords{DistIndex(j,i)}(:,1) Coords{DistIndex(j,i)}(:,2)];
-        x = isempty(intersect(v,u));
+        x = isempty(intersect(Coords{i}, Coords{DistIndex(j, i)}));
         ConnectionMat(i,DistIndex(j,i)) = 1 - x;
     end
-    v = [];
-    u = [];
 end
-NearClass = zeros(3485,1);
-for i = 1:3485
+
+
+NearClass = zeros(numPoints,1);
+for i = 1:numPoints
     v = find(ConnectionMat(:,i));
     if (sum(Class(v))/length(Class(v))) >= 0.5
         NearClass(i) = 1;
@@ -37,8 +39,3 @@ end
 
 Structure = sum(NearClass)/length(Class)
 return
-
-
-
-
-
