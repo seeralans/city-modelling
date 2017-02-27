8f5f508b43ecc5cfd6c5a962a10b152bb71ead6c
Ben Tambling <bt14551@my.bristol.ac.uk>
2017-02-27 11:55:56 +0000

    Looks at KS data

--- loadData.m
+++ loadData.m
@@ -3,7 +3,7 @@ function cen = loadData()
 % For consistency sake keep the current directory structure
 
 	% change this line to load different data
-	fileList = dir('Data/Data/Reduced_Census/Census/qs*');
+	fileList = dir('Data/Data/Reduced_Census/Census/ks*');
 
 
 	for i = 1:length(fileList)
