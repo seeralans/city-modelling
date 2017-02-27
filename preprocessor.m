b07ae57e7d4c1ef705d167a60da015e22835e9d4
Ben Tambling <bt14551@my.bristol.ac.uk>
2017-02-27 17:03:21 +0000

    Normalises and apples pca

--- preprocessor.m
+++ preprocessor.m
@@ -57,7 +57,7 @@ end
 T = sum(abs(RS_coeff), 2);
 [sort_T, ind] = sort(T);
 
-clear diff height i width xMax xMin normDataMat cen T
+clear diff height i width xMax xMin dataMat cen T
 % test = zeros(814, 2);
 %
 % idx2(1,1)
