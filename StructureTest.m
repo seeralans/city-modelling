543f298a9c0471f3e4e939836f491f3fd462a7c1
lbothwell <lb14292@my.bristol.ac.uk>
2017-02-28 12:58:50 +0000

    MultiClass code

--- StructureTest.m
+++ StructureTest.m
@@ -1,5 +1,6 @@
-%% New code, compare neighbouring areas
-function Structure = StructureTest(Class, Coords, ConnectionMat)
+% New code, compare neighbouring areas
+% Binary class input needs to be [0,1], multiclass can be any number.
+function [Structure] = StructureTest(Class, Coords, ConnectionMat)
 
 numPoints = length(Coords);
 
@@ -8,15 +9,35 @@ Midpoint = zeros(numPoints,2);
 for i = 1:numPoints
     Midpoint(i,:) = findCentroid(Coords{i});
 end
-NearClass = zeros(numPoints,1);
-for i = 1:numPoints
-    v = find(ConnectionMat(:,i));
-    if (sum(Class(v))/length(Class(v))) >= 0.5
-        NearClass(i) = 1;
-    else
-        NearClass(i) = 0;
+
+if length(unique(Class)) == 2
+    NearClass = zeros(numPoints,1);
+    for i = 1:numPoints
+        v = find(ConnectionMat(:,i));
+        NearClass(i) = (sum(Class(v))/length(v));
     end
+    Structure = sum(sum((NearClass - Class).^2))/length(Class); % Mean sqrd error
+elseif length(unique(Class)) > 2
+    NearClass = zeros(numPoints,length(unique(Class)));
+    C = unique(Class);
+    for j  = 1:length(C)
+        for i = 1:numPoints
+            v = find(ConnectionMat(:,i));
+            NearClass(i,j) = sum(Class(v) == C(j))/length(v);
+        end
+    end
+    Class2 = zeros(numPoints,length(unique(Class)));
+    for i = 1:numPoints
+        for j = 1:length(C)
+            if Class(i) == C(j)
+                Class2(i,j) = 1;
+            end
+        end
+    end
+    NearClass(isnan(NearClass)) = 0;
+    Structure = sum(sum((NearClass - Class2).^2))/ length(Class); % Mean sqrd error
+elseif length(unique(Class)) < 2
+    disp('Class error - one class')
 end
 
-Structure = sum(NearClass)/length(Class)
 return
