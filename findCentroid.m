7827bc9c722463ce97a4d227b93c3126e7c65bcc
lbothwell <lb14292@my.bristol.ac.uk>
2017-03-02 15:15:51 +0000

    output update

--- findCentroid.m
+++ findCentroid.m
@@ -8,6 +8,12 @@ for i = 1:length(A)-1
 end
 Cx = -Cx/(Area*6);
 Cy = -Cy/(Area*6);
+if Cx > -2.2 || Cx < -3.2
+    Cx = mean(A(:,1));
+end
+if Cy > -51.7 || Cy < -51.25
+    Cy = mean(A(:,2));
+end
 C = [Cx, Cy];
 %scatter(Cx,Cy)
 return
