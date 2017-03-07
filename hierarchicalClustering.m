c900f7f13aed3f9eb2ef3a1e62dfcf4e6578893e
Ben Menzies <bm14539@my.bristol.ac.uk>
2017-03-07 13:48:58 +0000

    Neatened final version where reclustering is removed

--- hierarchicalClustering.m
+++ hierarchicalClustering.m
@@ -1,49 +1,46 @@
 % run preprocessor.m before running this script
-% eg command window entry: hierarchicalClustering(normDataMat(:,ind(end-20:end)),4)
+% run with [clusterIDX, Class, Structure] = hierarchicalClustering(score(:,1:150),6)
 
-function [clusterIDX, reclusterIDX, Class] = hierarchicalClustering(data, numClusters)
+function [clusterIDX, Class, Structure] = hierarchicalClustering(data, numClusters)
 
 load Coords
+load ConnectionMat
 [height, ~] = size(data);
 
 %% Options
-cutoff = numClusters; % choose cutoff threshold for where to cut the hierarchical tree
 % choose linkage from:
 % 'average', 'centroid', 'complete', 'median', 'single', 'ward', 'weighted'
 % complete seems to be best
 linkageType = 'complete'; 
-% choose the top n most important features found from pca
-% n = 20;
 
 %% Hierarchical clustering
-%index = ind; % need this line as matlab gets confused otherwise
-clusterIDX = clusterdata(data,'maxclust',cutoff,'linkage', linkageType);
-% run hierarchical clustering again to get only 2 clusters (city/noncity hopefully)
-cutoff2 = 2;
-reclusterIDX = clusterdata(clusterIDX,'maxclust',cutoff2,'linkage', linkageType);
+clusterIDX = clusterdata(data,'maxclust',numClusters,'linkage', linkageType);
+
 Class = zeros(height,1);
 for i = 1:height
-    Class(i) = reclusterIDX(clusterIDX(i));
+    Class(i) = clusterIDX(i);
 end
-Class =  Class - 1 ;
 
 %% Plot silhouette to show how crap our clusters are
 figure();
-silhouette(data,reclusterIDX);
+silhouette(data,clusterIDX);
 
 %% Plot map
-figure();
+FigHandle = figure('Position', [100, 100, 800, 600],'Name','Map - Hierarchical');
+set(gca,'fontsize',10)
+xlabel('latitude')
+ylabel('longnitude')
+title('Using PCA and Hierarchical clustering to cluster output areas')
+
 for i = 1:height
     a = Coords{i}(:,1);
     b = Coords{i}(:,2);
-    if Class(i) == 1
-        patch(a,b,[0.66 0.66 0.66])
-        hold on
-    else
-        patch(a,b,[1 1 1])
-        hold on
-    end
+    j = Class(i)/numClusters;
+    col = [abs(1-j) abs(1-j) abs(1-j)];
+    patch(a,b,col)
+    hold on
 end
 hold off;
 
+Structure = StructureTest(Class, Coords, ConnectionMat)
 end
