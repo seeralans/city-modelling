972488add77adb4da1b164c2d0d15f1a8edb43de
lbothwell <lb14292@my.bristol.ac.uk>
2017-03-08 11:28:48 +0000

    Silhoutte update

--- KmeansClustering.m
+++ KmeansClustering.m
@@ -14,15 +14,15 @@ for i = 1:k
 
 end
 Class = zeros(numPoints,1);
-k2 = 7; % no. of classes
+k2 = 2; % no. of classes
 [reclusterIDX,~] = kmeans(Centriods,k2);
 for i = 1:numPoints  
     Class(i) = reclusterIDX(clusterIDX(i));
 end
 
 figure();
-silhouette(score(:,1:150),clusterIDX);
-s = silhouette(score(:,1:150),clusterIDX);
+silhouette(score(:,1:150),reclusterIDX);
+s = silhouette(score(:,1:150),reclusterIDX);
 meanSil = mean(s)
 
 FigHandle = figure('Position', [100, 100, 800, 600],'Name','Map - Kmeans');
